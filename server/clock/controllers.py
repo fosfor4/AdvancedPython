7a40a975d6588dbc3b9f06642844a587d2d11602
fosfor4 <fosfor@mail.rugit config --global user.email fosfor@mail.rugit config --global user.email fosfor@mail.ru>
2019-08-01 22:49:41 +0300

    2

--- server/clock/controllers.py
+++ server/clock/controllers.py
@@ -1,6 +1,8 @@
 import time
 from protocol import make_response
+from decorators import logged
 
+@logged
 def send_time(request):
     timestr = 'Server time: ' + time.ctime(time.time())
     return make_response(request, 200, timestr)
